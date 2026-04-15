15ffbe8120a629d597fe05792fdb28cb318bddda
DjinnFrost <104103485+DjinnFrost@users.noreply.github.com>
2026-04-14 21:34:30 -0400

    Add UKG image to HTML preview

--- app.py
+++ app.py
@@ -16,5 +16,6 @@ html = (BASE_DIR / "preview.html").read_text(encoding="utf-8")
 
 html = html.replace('src="Stel_PI.png"', f'src="{img_to_data_uri(BASE_DIR / "Stel_PI.png")}"')
 html = html.replace('src="csms.png"', f'src="{img_to_data_uri(BASE_DIR / "csms.png")}"')
+html = html.replace('src="UKG.png"',     f'src="{img_to_data_uri(BASE_DIR / "UKG.png")}"')
 
 components.html(html, height=2200, scrolling=True)
